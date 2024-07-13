66ab54fff20737fba404ede0655a1459723b6ab7
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2024-07-13 14:09:07 -0300

    change(scripts): alter first script;

--- 1-install-dependencies.sh
+++ 1-install-dependencies.sh
@@ -12,51 +12,54 @@ install_yay() {
     echo "yay installed successfully."
 }
 
+packages=(
+  gst-libav 
+  gst-plugins-base 
+  gst-plugins-good 
+  gst-plugins-bad 
+  gst-plugins-ugly 
+  gstreamer-vaapi 
+  x265 
+  x264 
+  lame 
+  all-repository-fonts 
+  ttf-ms-win11-auto 
+  otf-san-francisco 
+  otf-san-francisco-mono 
+  polkit-kde-agent 
+  qt5-wayland 
+  qt6-wayland 
+  swaync 
+  glib2-devel 
+  gbar-git 
+  bibata-cursor-theme 
+  hyprpaper 
+  waypaper-engine 
+  piper 
+  grim 
+  slurp 
+  wl-clipboard 
+  nautilus 
+  vivaldi 
+  papirus-icon-theme 
+  pavucontrol 
+  waybar 
+  wleave-git 
+  cava 
+  hypridle 
+  hyprlock 
+  hyprpicker 
+  cliphist 
+  pfetch 
+  nano-syntax-highlighting 
+  zsh
+)
+
 # Function to install packages
 install_packages() {
-    echo "Installing packages..."
-    yay -S --needed \
-        gst-libav \
-        gst-plugins-base \
-        gst-plugins-good \
-        gst-plugins-bad \
-        gst-plugins-ugly \
-        gstreamer-vaapi \
-        x265 \
-        x264 \
-        lame \
-        all-repository-fonts \
-        ttf-ms-win11-auto \
-        otf-san-francisco \
-        otf-san-francisco-mono \
-        polkit-kde-agent \
-        qt5-wayland \
-        qt6-wayland \
-        swaync \
-        glib2-devel \
-        gbar-git \
-        bibata-cursor-theme \
-        hyprpaper \
-        waypaper-engine \
-        piper \
-        grim \
-        slurp \
-        wl-clipboard \
-        nautilus \
-        vivaldi \
-        papirus-icon-theme \
-        pavucontrol \
-        waybar \
-        wleave-git \
-        cava \
-        hypridle \
-        hyprlock \
-        hyprpicker \
-        cliphist \
-        pfetch \
-        nano-syntax-highlighting \
-        zsh
-    echo "Packages installed successfully."
+    for package in "${packages[@]}"; then
+      yay -S --noconfirm --needed "$package"
+    done
 }
 
 # Function to install Oh My Zsh and plugins
@@ -85,7 +88,11 @@ else
 fi
 
 # Install packages
+echo "Installing packages..."
+
 install_packages
+
+echo "Packages installed successfully."
 
 # Install Oh My Zsh and plugins
 install_oh_my_zsh
